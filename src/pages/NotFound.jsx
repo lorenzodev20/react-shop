fb3da4214782d3fa061149d94f626141f7078ea2
Lorenzo Rojo <lorenzorojo12@gmail.com>
2022-06-20 20:29:58 -0500

    Avance del proyecto

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../styles/NotFound.scss';
+
+
+const NotFound = () => {
+    return (
+        <div className="not-found">
+            <div className="container-center">
+                <h1 className="title">Error 404</h1>
+                <p className="subtitle">Página no encontrada</p>
+                <a href="" className='subtitle'>Volver al inicio</a>
+            </div>
+        </div>
+
+    );
+}
+
+export default NotFound;
