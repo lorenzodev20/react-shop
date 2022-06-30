1078642c0c5ecc1483679db5451b7d00adeb45d0
Lorenzo Rojo <lorenzorojo12@gmail.com>
2022-06-29 19:29:11 -0500

    Agregando alias y ajustando llamado de imagenes en los componentes

---
+++ src/containers/ProductList.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ProductItem from '@components/ProductItem';
+import '@styles/ProductList.scss';
+
+const ProductList = () => {
+	return (
+		<section className="main-container">
+			<div className="ProductList">
+				<ProductItem />
+				<ProductItem />
+				<ProductItem />
+				<ProductItem />
+				<ProductItem />
+				<ProductItem />
+				<ProductItem />
+				<ProductItem />
+				<ProductItem />
+				<ProductItem />
+			</div>
+		</section>
+	);
+}
+
+export default ProductList;
